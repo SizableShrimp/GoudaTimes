49b4b8dfabe470b3d57912d1191127d283062d04
spin-ice <tacos12368@tutanota.com>
2023-11-04 17:00:29 -0500

    lets goooo

--- scraper/article.py
+++ scraper/article.py
@@ -4,9 +4,10 @@ from dataclasses import dataclass
 class ArticleNPK: # NPK = No Primary Key, See database schema
     title: str
     nofield_abstract: str # Does not appear in the database, just to give the AI more context
-    date_written: str
+    date_written: str | None
     by_line: str
-    header_image_url: str
+    header_image_url: str | None
+    header_image_attribution: str | None
     source_domain: str
     sorted_categories: dict[str, list[str]]
-    content: str
+    content: str # Can be "<< not yet generated >>"
--- scraper/get_top_stories.py
+++ scraper/get_top_stories.py
@@ -5,6 +5,13 @@ from article import ArticleNPK
 
 nyt_api_key = os.environ["NYT_DEV_API_KEY"]
 
+nytimes_facet_names = {
+    "des_facet": "description",
+    "org_facet": "organization",
+    "per_facet": "person",
+    "geo_facet": "location",
+}
+
 def get_top_stories() -> list[ArticleNPK]:
     resp = req.get(f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={nyt_api_key}")
     assert resp.status_code == 200
@@ -12,12 +19,24 @@ def get_top_stories() -> list[ArticleNPK]:
     for result in resp.json()["results"]:
         if result["item_type"] != "Article":
             continue
+        if result["byline"] == "":
+            continue
+        has_image  = (len(result["multimedia"]) > 0)
+        has_image &= (result["multimedia"][0]["copyright"] != "")
         ret.append(ArticleNPK(
             result["title"],
             result["abstract"],
             result["published_date"][:10],
             result["byline"],
-            result["multimedia"][0]["url"],
+            result["multimedia"][0]["url"]       if has_image else None,
+            result["multimedia"][0]["copyright"] if has_image else None,
+            "nytimes.com",
+            {
+                nytimes_facet_names[k]: v
+                for k, v in result.items()
+                if k.endswith("_facet") and (k in nytimes_facet_names)
+            },
+            "<< not yet generated >>",
         ))
     return ret
 
