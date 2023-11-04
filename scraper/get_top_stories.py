34bf4b320d4babe8bd08393b59820c61ecff05af
spin-ice <tacos12368@tutanota.com>
2023-11-04 16:47:30 -0500

    saving work

--- scraper/get_top_stories.py
+++ scraper/get_top_stories.py
@@ -1,4 +1,25 @@
 import requests as req
+import os
+
+from article import ArticleNPK
+
+nyt_api_key = os.environ["NYT_DEV_API_KEY"]
 
 def get_top_stories() -> list[ArticleNPK]:
-    pass # TODO
+    resp = req.get(f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={nyt_api_key}")
+    assert resp.status_code == 200
+    ret = []
+    for result in resp.json()["results"]:
+        if result["item_type"] != "Article":
+            continue
+        ret.append(ArticleNPK(
+            result["title"],
+            result["abstract"],
+            result["published_date"][:10],
+            result["byline"],
+            result["multimedia"][0]["url"],
+        ))
+    return ret
+
+if __name__ == "__main__":
+    print(get_top_stories())
