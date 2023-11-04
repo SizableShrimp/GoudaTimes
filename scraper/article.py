414cba52056c472e58d947c4e30a90409a8fb46d
spin-ice <tacos12368@tutanota.com>
2023-11-04 16:47:30 -0500

    saving work

--- scraper/article.py
+++ scraper/article.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 
 @dataclass
-class ArticleNPK: # NPK = No Primary Key
+class ArticleNPK: # NPK = No Primary Key, See database schema
     title: str
+    nofield_abstract: str # Does not appear in the database, just to give the AI more context
     date_written: str
     by_line: str
     header_image_url: str
