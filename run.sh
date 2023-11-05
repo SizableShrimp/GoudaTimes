4d6599f53b9f60605e3e17659b33111ddc1b18d1
SizableShrimp <sizableshrimp@sizableshrimp.me>
2023-11-04 20:24:56 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+# Set env vars
+export $(grep -v '^#' .env | xargs)
+
 # Backend
 cd backend
 env $(cat .env | xargs) ./gradlew bootRun
 cd ..
+
+# Unset env vars
+unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
