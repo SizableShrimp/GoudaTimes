037bebc5e7df2905e711693ef111ba989269363f
Tsuni <akamitsunami@gmail.com>
2023-11-05 10:57:54 -0600

    add github + about links in footer

--- frontend/src/routes/footer.svelte
+++ frontend/src/routes/footer.svelte
@@ -6,6 +6,8 @@
     <div class="divider" />
     <div class="divider" />
     <img src={GoudaTimesImg} alt="" />
+    <a href="/about">About</a>
+    <a href="https://github.com/SizableShrimp/TheCheesyTimes">GitHub</a>
 </div>
 
 <style>
@@ -27,5 +29,10 @@
         width: 100%;
         height: 1px;
         background-color: white;
+    }
+    a {
+        color: white;
+        margin-top: 0.5rem;
+        display: block;
     }
 </style>
