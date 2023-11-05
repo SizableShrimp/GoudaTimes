fc18d13e2c78719c4be344ea54e27640242e0741
SizableShrimp <sizableshrimp@sizableshrimp.me>
2023-11-04 21:31:17 -0500

    Move around endpoints and add CORS

--- backend/src/main/java/me/sizableshrimp/thecheesytimes/backend/ArticlesController.java
+++ backend/src/main/java/me/sizableshrimp/thecheesytimes/backend/ArticlesController.java
@@ -1,14 +1,17 @@
 package me.sizableshrimp.thecheesytimes.backend;
 
+import jakarta.persistence.EntityNotFoundException;
 import me.sizableshrimp.thecheesytimes.backend.db.Article;
 import me.sizableshrimp.thecheesytimes.backend.db.ArticleRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/articles")
+@RequestMapping(path = "/api/articles")
 public class ArticlesController {
     private final ArticleRepository articleRepository;
 
@@ -17,8 +20,16 @@ public class ArticlesController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping(path = "/all")
+    @CrossOrigin
+    @GetMapping("")
     public @ResponseBody Iterable<Article> index() {
-        return articleRepository.findAll();
+        return this.articleRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public Article findById(@PathVariable Integer id) {
+        return this.articleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Could not find article with id " + id));
     }
 }
