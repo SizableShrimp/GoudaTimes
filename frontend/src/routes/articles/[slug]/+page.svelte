7e707230e2e15f6ff0958137272286977b5435ea
Sam Nesler <snesler@wisc.edu>
2023-11-05 01:46:23 -0500

    hide link in dark mode!!

--- frontend/src/routes/articles/[slug]/+page.svelte
+++ frontend/src/routes/articles/[slug]/+page.svelte
@@ -1,12 +1,20 @@
 <script>
+    import { onMount } from 'svelte';
+    import { darkModeStore } from '../../../stores';
     /** @type {import('./$types').PageData} */
 	export let data;
     const formatted = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(new Date(data.article.dateWritten));
+    let isDark = false;
+    onMount(() => {
+        darkModeStore.subscribe((value) => {
+            isDark = value;
+        });
+    });
 </script>
 
 <div class="article">
     <h1>{data.article.title}</h1>
-    <p>{formatted}</p>
+    <i>{formatted}</i>
     {#if data.article.headerImageUrl}
         <img src={data.article.headerImageUrl} alt={data.article.headerImageAttribution}/>
         <small>{data.article.headerImageAttribution}</small>
@@ -14,7 +22,9 @@
     {#each data.article.content.split("\n") as paragraph}
         <p>{paragraph}</p>
     {/each}
-    <a href={data.article.originalUrl}>Via the New York Times</a>
+    {#if !isDark}
+        <a href={data.article.originalUrl}>Via the New York Times</a>
+    {/if}
 </div>
 
 <style>
